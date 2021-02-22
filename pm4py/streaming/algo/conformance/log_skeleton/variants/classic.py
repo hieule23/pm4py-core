0e66d3678695d89b075a1db0245200f41b452e54
HieuLe <hieu.le@rwth-aachen.de>
2021-02-22 17:56:05 +0100

    bugs fixed

--- pm4py/streaming/algo/conformance/log_skeleton/variants/classic.py
+++ pm4py/streaming/algo/conformance/log_skeleton/variants/classic.py
@@ -1,7 +1,7 @@
 from pm4py.util import constants, exec_utils, xes_constants
-from pm4py.streaming.algo.interface import StreamingAlgorithm
 import logging
-from streaming.algo.conformance.log_skeleton.outputs import Outputs
+from pm4py.streaming.algo.conformance.log_skeleton.outputs import Outputs
+from pm4py.streaming.algo.interface import StreamingAlgorithm
 
 
 class Parameters:
@@ -9,7 +9,7 @@ class Parameters:
     ACTIVITY_KEY = constants.PARAMETER_CONSTANT_ACTIVITY_KEY
 
 
-class LogStreamingConformance(object):
+class LogStreamingConformance(StreamingAlgorithm):
     def __init__(self, log, parameters=None):
         if parameters is None:
             parameters = {}
@@ -18,17 +18,15 @@ class LogStreamingConformance(object):
                                                        xes_constants.DEFAULT_NAME_KEY)
         self.log = log
         self.build_dictionary()
+        StreamingAlgorithm.__init__(self)
 
     def build_dictionary(self):
         self.last_act_dict = {}
         self.freq_act_dict = {}
         self.act_in_trace_dict = {}
-        self.deviations = {Outputs.DIRECTLY_FOLLOWS.value: [],
-                           Outputs.ACTIV_FREQ.value: [],
-                           Outputs.NEVER_TOGETHER.value: [],
-                           Outputs.ALWAYS_BEFORE.value: []}
+        self.deviations = {}
 
-    def receive(self, event):
+    def _process(self, event):
         """
                 Execution of the object when receive an event from live stream
                 Parameters
@@ -39,6 +37,7 @@ class LogStreamingConformance(object):
                 -------
                     Check all the conditions in log_skeleton skeleton constraints
         """
+
         self.check(event)
         self.update_dict(event)
 
@@ -52,10 +51,15 @@ class LogStreamingConformance(object):
         return self.deviations
 
     def update_deviation(self, case, error):
+        key = list(error.items())[0][0]
+        value = list(error.items())[0][1]
         if case in self.deviations:
-            self.deviations[case].append(error)
+            if key in self.deviations[case]:
+                self.deviations[case][key].append(value)
+            else:
+                self.deviations[case][key] = [value]
         else:
-            self.deviations[case] = [error]
+            self.deviations[case] = {key: [value]}
 
     def check(self, event):
         """
@@ -68,13 +72,20 @@ class LogStreamingConformance(object):
         -------
 
         """
+
         case = event[self.case_id_key] if self.case_id_key in event else None
         activity = event[self.activity_key] if self.activity_key in event else None
+
         if case is not None and activity is not None:
+
             self.verify_directly_follow(event)
+
             self.verify_frequency(event)
+
             self.verify_always_before(event)
+
             self.verify_never_together(event)
+
         else:
             self.message_case_or_activity_not_in_event(event)
 
@@ -90,36 +101,57 @@ class LogStreamingConformance(object):
             3 updated dictionaries
 
         """
+
         case = event[self.case_id_key] if self.case_id_key in event else None
         activity = event[self.activity_key] if self.activity_key in event else None
         if case is not None and activity is not None:
             self.update_last_act_dict(event)
+
             self.update_freq_act_dict(event)
+
             self.update_act_in_trace_dict(event)
 
     def verify_directly_follow(self, event):
         case = event[self.case_id_key]
         activity = event[self.activity_key]
+
         if case in self.last_act_dict:
+
             act_1 = self.last_act_dict[case]
+
             act_2 = activity
             if (act_1, act_2) not in self.log[Outputs.DIRECTLY_FOLLOWS.value]:
-                self.message_directly_follows_not_fit(act_2, act_1)
-                error = (Outputs.DIRECTLY_FOLLOWS.value, (activity, act_1))
+                self.message_directly_follows_not_fit(case, act_2, act_1)
+
+                error = {Outputs.DIRECTLY_FOLLOWS.value: (activity, act_1)}
+
                 self.update_deviation(case, error)
 
+        else:
+            return
+
     def verify_frequency(self, event):
+
         case = event[self.case_id_key]
         activity = event[self.activity_key]
         if case in self.freq_act_dict:
+
             if activity in self.freq_act_dict[case]:
+
                 act_freq = self.freq_act_dict[case][activity] + 1
-                if act_freq not in self.log[Outputs.ACTIV_FREQ.value]:
-                    max_allow = max(self.log[Outputs.ACTIV_FREQ.value])
+
+                if act_freq not in self.log[Outputs.ACTIV_FREQ.value][activity]:
+
+                    max_allow = max(self.log[Outputs.ACTIV_FREQ.value][activity])
+
                     if act_freq > max_allow:
                         self.message_freq_act_not_fit(case, activity, max_allow)
-                        error = (Outputs.ACTIV_FREQ.value, (activity, max_allow))
+                        error = {Outputs.ACTIV_FREQ.value: (activity, max_allow)}
                         self.update_deviation(case, error)
+
+        else:
+
+            return
 
     def verify_always_before(self, event):
         case = event[self.case_id_key]
@@ -129,27 +161,33 @@ class LogStreamingConformance(object):
             act_occurred = set(self.act_in_trace_dict[case])
             if not must_occur.issubset(act_occurred):
                 not_occurred = must_occur - act_occurred
-                error = (Outputs.ALWAYS_BEFORE.value, (activity, not_occurred))
+                error = {Outputs.ALWAYS_BEFORE.value: (activity, not_occurred)}
                 self.message_always_before_not_fit(case, activity, not_occurred)
                 self.update_deviation(case, error)
+        else:
+            return
 
     def verify_never_together(self, event):
         case = event[self.case_id_key]
         activity = event[self.activity_key]
+
+
         if case in self.act_in_trace_dict:
             not_occur = set([x[1] for x in self.log[Outputs.NEVER_TOGETHER.value] if x[0] == activity] +
                             [x[0] for x in self.log[Outputs.NEVER_TOGETHER.value] if x[1] == activity])
             act_occurred = set(self.act_in_trace_dict[case])
             if len(act_occurred.intersection(not_occur)) != 0:
                 deviation = act_occurred.intersection(not_occur)
-                error = (Outputs.NEVER_TOGETHER.value, (activity, deviation))
+                error = {Outputs.NEVER_TOGETHER.value: (activity, deviation)}
                 self.message_never_together_not_fit(case, activity, deviation)
                 self.update_deviation(case, error)
+        else:
+            return
 
     def update_last_act_dict(self, event):
         case = event[self.case_id_key]
         activity = event[self.activity_key]
-        self.act_in_trace_dict[case] = activity
+        self.last_act_dict[case] = activity
 
     def update_freq_act_dict(self, event):
         case = event[self.case_id_key]
@@ -163,11 +201,16 @@ class LogStreamingConformance(object):
                 self.freq_act_dict[case][activity] += 1
 
     def update_act_in_trace_dict(self, event):
+
         case = event[self.case_id_key]
+
         activity = event[self.activity_key]
+
         if case not in self.act_in_trace_dict:
+
             self.act_in_trace_dict[case] = [activity]
         else:
+
             self.act_in_trace_dict[case].append(activity)
 
     def message_case_or_activity_not_in_event(self, event):
